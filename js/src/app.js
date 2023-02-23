84f4b064dfb4ca0b0584628d4b0159d7a51b02b5
Chamith Jayaweera <chamithlkml@gmail.com>
2023-02-23 14:11:47 +0000

    Sample codes introduced to javascripts to speed up the learning cycle

--- js/src/app.js
+++ js/src/app.js
@@ -1,22 +1,80 @@
 $(window).on('load', function(){
 $(".signup").css("display","none");
 
+// Navigate user to SignUp form
 $("#linkcreatAcc").click(function(){
    $(".signin").css("display","none");
    $(".signup").css("display","");
 });
 
+// Navigate user to SignIn form
 $("#linkloging").click(function(){
     $(".signin").css("display","");
     $(".signup").css("display","none");
  });
- 
+
+// Code sample to call a method on its value changed event
+// $(document).on("change","#fname", function(){
+//     console.log('firstname changed');
+// });
+
+var my_int = 1;
+var my_sstr = 'asdfadsf';
+let non_empty_fields = ['fname', 'lname'];// Javascript array
+// console.log(non_empty_fields[0]);
+// console.log(non_empty_fields.length);
+// non_empty_fields.push()
+// non_empty_fields.pop{}
+// non_empty_fields.shift
+// non_empty_fields.unshift
+
+// loop through array, Iteration
+// non_empty_fields.forEach(function(el){
+//     console.log(el);
+// });
+
+let first_name_field = {
+    id: 'fname',
+    name: 'First Name',
+    error_id: 'fname_error'
+};
+
+// console.log(check_empty_fields);
+// console.log(check_empty_fields['id']);
+// console.log(check_empty_fields['error_id']);
+
+let check_for_emptyness = [
+    {
+        id: 'fname',
+        name: 'First Name',
+        error_id: 'fname_error'
+    },
+    {
+        id: 'lname',
+        name: 'Last Name',
+        error_id: 'lname_error'
+    }
+];
+
+check_for_emptyness.forEach(function(field){
+    console.log('id: ' + field['id']);
+    console.log('name: ' + field['name']);
+    console.log('error id: '+field['error_id']);
+});
 
 
-
-
+non_empty_fields.forEach(function(input_id){
+    $(document).on("change", '#'+input_id, function(){
+        // Get value of the input example 1
+        // let input_value = $('#'+input_id).val();
+        // Get value of the input example 2
+        let input_value = $(this).val();
+        console.log('input value: '+input_value);
+    });
+});
 
 let is_form_validated = function(){
+
    let first_name_validation = function(){
         let first_name = $("#fname").val();
         if(first_name==""){
